b816fbf10b5c7dbd8f73d6d73d039daa4039fb24
owent <admin@owent.net>
2021-04-26 21:25:36 +0800

    Add GTest and benchmark builder
    Signed-off-by: owent <admin@owent.net>

--- ports/grpc/upb.cmake
+++ ports/grpc/upb.cmake
@@ -3,3 +3,5 @@
 # git@github.com:protocolbuffers/upb.git
 
 include_guard(GLOBAL)
+
+# It's already included in grpc/third_party/upb , there is no need to import it again.
---
+++ ports/test/gtest.cmake
@@ -0,0 +1,87 @@
+# An xUnit test framework.
+# https://github.com/google/googletest/
+# git@github.com:google/googletest.git
+
+include_guard(GLOBAL)
+
+# =========== third party GTest ==================
+macro(PROJECT_THIRD_PARTY_GTEST_IMPORT)
+  if(TARGET GTest::gtest)
+    message(STATUS "Dependency(${PROJECT_NAME}): Target GTest::gtest found")
+  endif()
+  if(TARGET GTest::gtest_main)
+    message(STATUS "Dependency(${PROJECT_NAME}): Target GTest::gtest_main found")
+  endif()
+  if(TARGET GTest::gmock)
+    message(STATUS "Dependency(${PROJECT_NAME}): Target GTest::gmock found")
+  endif()
+  if(TARGET GTest::gmock_main)
+    message(STATUS "Dependency(${PROJECT_NAME}): Target GTest::gmock_main found")
+  endif()
+endmacro()
+
+if(NOT TARGET GTest::gtest AND NOT TARGET GTest::gtest_main)
+  if(VCPKG_TOOLCHAIN)
+    find_package(GTest QUIET CONFIG)
+    project_third_party_gtest_import()
+  endif()
+
+  if(NOT TARGET GTest::gtest AND NOT TARGET GTest::gtest_main)
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_VERSION)
+      set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_VERSION "release-1.10.0")
+    endif()
+
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_GIT_URL)
+      set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_GIT_URL
+          "https://github.com/google/googletest.git")
+    endif()
+
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_DIR)
+      project_third_party_get_build_dir(
+        ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_DIR "gtest"
+        ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_VERSION})
+    endif()
+
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_OPTIONS)
+      set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_OPTIONS
+          "-DCMAKE_POSITION_INDEPENDENT_CODE=YES" "-DBUILD_GMOCK=ON" "-DINSTALL_GTEST=ON")
+    endif()
+    if(MSVC)
+      list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_OPTIONS
+           "-DCMAKE_DEBUG_POSTFIX=d")
+    endif()
+    project_third_party_append_find_root_args(
+      ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_OPTIONS)
+    project_third_party_append_build_shared_lib_var(
+      ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_OPTIONS BUILD_SHARED_LIBS)
+
+    find_configure_package(
+      PACKAGE
+      GTest
+      BUILD_WITH_CMAKE
+      FIND_PACKAGE_FLAGS
+      CONFIG
+      CMAKE_INHIRT_BUILD_ENV
+      CMAKE_INHIRT_FIND_ROOT_PATH
+      CMAKE_FLAGS
+      ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_OPTIONS}
+      WORKING_DIRECTORY
+      "${PROJECT_THIRD_PARTY_PACKAGE_DIR}"
+      BUILD_DIRECTORY
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_BUILD_DIR}"
+      PREFIX_DIRECTORY
+      "${PROJECT_THIRD_PARTY_INSTALL_DIR}"
+      SRC_DIRECTORY_NAME
+      "gtest-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_VERSION}"
+      GIT_BRANCH
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_VERSION}"
+      GIT_URL
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_GTEST_GIT_URL}")
+
+    if(TARGET GTest::gtest OR TARGET GTest::gtest_main)
+      project_third_party_gtest_import()
+    endif()
+  endif()
+else()
+  project_third_party_gtest_import()
+endif()
