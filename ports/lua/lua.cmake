33f504b428f76e5795bb6349601b49f6a1054aa0
owent <admin@owent.net>
2021-05-13 15:04:13 +0800

    Add civetweb Add prometheus-cpp Change lua to install target instead of subdirectory
    Signed-off-by: owent <admin@owent.net>

--- ports/lua/lua.cmake
+++ ports/lua/lua.cmake
@@ -1,32 +1,25 @@
 include_guard(GLOBAL)
 
 macro(PROJECT_THIRD_PARTY_LUA_IMPORT)
-  if(BUILD_SHARED_LIBS OR ATFRAMEWORK_USE_DYNAMIC_LIBRARY)
-    if(TARGET lua::liblua-dynamic)
-      echowithcolor(COLOR GREEN
-                    "-- Dependency(${PROJECT_NAME}): Lua found target lua::liblua-dynamic")
-      list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_PUBLIC_LINK_NAMES lua::liblua-dynamic)
-      if(NOT TARGET lua)
-        if(TARGET lua_DYNAMIC)
-          add_library(lua ALIAS lua_DYNAMIC)
-        else()
-          add_library(lua ALIAS lua::liblua-dynamic)
-        endif()
-      endif()
+  if(TARGET lua::liblua-dynamic)
+    echowithcolor(COLOR GREEN
+                  "-- Dependency(${PROJECT_NAME}): Lua found target lua::liblua-dynamic")
+    set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_LINK_NAME lua::liblua-dynamic)
+    if(NOT TARGET lua)
+      add_library(lua INTERFACE IMPORTED)
+      set_target_properties(lua PROPERTIES INTERFACE_LINK_LIBRARIES lua::liblua-dynamic)
     endif()
-  else()
-    if(TARGET lua::liblua-static)
-      echowithcolor(COLOR GREEN
-                    "-- Dependency(${PROJECT_NAME}): Lua found target lua::liblua-static")
-      list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_PUBLIC_LINK_NAMES lua::liblua-static)
-      if(NOT TARGET lua)
-        if(TARGET lua_STATIC)
-          add_library(lua ALIAS lua_STATIC)
-        else()
-          add_library(lua ALIAS lua::liblua-static)
-        endif()
-      endif()
+  elseif(TARGET lua::liblua-static)
+    echowithcolor(COLOR GREEN "-- Dependency(${PROJECT_NAME}): Lua found target lua::liblua-static")
+    set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_LINK_NAME lua::liblua-static)
+    if(NOT TARGET lua)
+      add_library(lua INTERFACE IMPORTED)
+      set_target_properties(lua PROPERTIES INTERFACE_LINK_LIBRARIES lua::liblua-static)
     endif()
+  endif()
+  if(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_LINK_NAME)
+    list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_PUBLIC_LINK_NAMES
+         ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_LINK_NAME})
   endif()
 endmacro()
 
@@ -41,19 +34,22 @@ if(NOT TARGET lua::liblua-static AND NOT TARGET lua::liblua-dynamic)
     project_third_party_get_build_dir(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_DIR "lua"
                                       ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION})
   endif()
+  if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_OPTIONS)
+    set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_OPTIONS
+        "-DCMAKE_POSITION_INDEPENDENT_CODE=ON" "-DCMAKE_DEBUG_POSTFIX=d")
+
+    project_third_party_append_build_shared_lib_var(
+      ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_OPTIONS BUILD_SHARED_LIBS)
+    project_third_party_append_build_static_lib_var(
+      ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_OPTIONS BUILD_STATIC_LIBS)
+  endif()
 
   if(NOT EXISTS "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_DIR}")
     file(MAKE_DIRECTORY "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_DIR}")
   endif()
   set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_GIT_ARGS
-      URL
-      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_GIT_URL}"
-      REPO_DIRECTORY
-      "${PROJECT_THIRD_PARTY_PACKAGE_DIR}/lua-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}"
-      BRANCH
-      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}"
-      CHECK_PATH
-      "luaconf.h")
+      GIT_BRANCH "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}" GIT_URL
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_GIT_URL}")
   if(CMAKE_CROSSCOMPILING)
     if(EXISTS
        "${CMAKE_CURRENT_LIST_DIR}/lua-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}.cross.patch"
@@ -61,21 +57,44 @@ if(NOT TARGET lua::liblua-static AND NOT TARGET lua::liblua-dynamic)
       list(
         APPEND
         ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_GIT_ARGS
-        PATCH_FILES
+        GIT_PATCH_FILES
         "${CMAKE_CURRENT_LIST_DIR}/lua-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}.cross.patch"
       )
     else()
       message(FATAL_ERROR "Must apply patch to disable call of system() on android/ios")
     endif()
   endif()
-  project_git_clone_repository(${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_GIT_ARGS})
-  add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/build-script"
-                   "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_DIR}")
+
+  find_configure_package(
+    PACKAGE
+    lua
+    BUILD_WITH_CMAKE
+    CMAKE_INHIRT_BUILD_ENV
+    CMAKE_INHIRT_BUILD_ENV_DISABLE_CXX_FLAGS
+    CMAKE_INHIRT_FIND_ROOT_PATH
+    CMAKE_FLAGS
+    ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_OPTIONS}
+    "-DLUA_TOP_SOURCE_DIR=${PROJECT_THIRD_PARTY_PACKAGE_DIR}/lua-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}"
+    WORKING_DIRECTORY
+    "${PROJECT_THIRD_PARTY_PACKAGE_DIR}"
+    PREFIX_DIRECTORY
+    "${PROJECT_THIRD_PARTY_INSTALL_DIR}"
+    SRC_DIRECTORY_NAME
+    "lua-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_VERSION}"
+    PROJECT_DIRECTORY
+    "${CMAKE_CURRENT_LIST_DIR}/build-script"
+    BUILD_DIRECTORY
+    "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_BUILD_DIR}"
+    ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_GIT_ARGS})
+
+  if(NOT TARGET lua::liblua-dynamic
+     AND NOT TARGET lua::liblua-static
+     AND NOT TARGET lua::liblua
+     AND NOT TARGET lua::lua
+     AND NOT TARGET lua::luac)
+    echowithcolor(COLOR YELLOW "-- Dependency(${PROJECT_NAME}): lua not found")
+  endif()
   project_third_party_lua_import()
 else()
   project_third_party_lua_import()
-endif()
-
-if(NOT TARGET lua)
-  message(FATAL_ERROR "Dependency(${PROJECT_NAME}): lua not found.")
 endif()
---
+++ ports/web/civetweb.cmake
@@ -0,0 +1,150 @@
+# Project mission is to provide easy to use, powerful, C (C/C++) embeddable web server with optional CGI, SSL and Lua support.
+# https://github.com/civetweb/civetweb/
+
+include_guard(GLOBAL)
+
+# =========== third party civetweb ==================
+function(PROJECT_THIRD_PARTY_CIVETWEB_PATCH_IMPORTED_TARGET TARGET_NAME)
+  unset(PATCH_REMOVE_RULES)
+  unset(PATCH_ADD_TARGETS)
+  if(TARGET OpenSSL::SSL
+     OR TARGET OpenSSL::Crypto
+     OR TARGET LibreSSL::TLS)
+    list(APPEND PATCH_REMOVE_RULES "(lib)?crypto" "(lib)?ssl")
+    list(APPEND PATCH_ADD_TARGETS ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CRYPT_LINK_NAME})
+  endif()
+
+  if(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_LINK_NAMES)
+    list(APPEND PATCH_REMOVE_RULES "(lib)?lua(_a)?")
+    list(APPEND PATCH_ADD_TARGETS ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LUA_LINK_NAME})
+  endif()
+  if(PATCH_REMOVE_RULES OR PATCH_ADD_TARGETS)
+    project_build_tools_patch_imported_link_interface_libraries(
+      ${TARGET_NAME} REMOVE_LIBRARIES ${PATCH_REMOVE_RULES} ADD_LIBRARIES ${PATCH_ADD_TARGETS})
+  endif()
+endfunction()
+
+macro(PROJECT_THIRD_PARTY_CIVETWEB_IMPORT)
+  if(TARGET civetweb::civetweb-cpp)
+    echowithcolor(COLOR GREEN
+                  "-- Dependency(${PROJECT_NAME}): civetweb found target civetweb::civetweb-cpp")
+    set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_LINK_NAME civetweb::civetweb-cpp)
+  elseif(TARGET civetweb::civetweb-c-library)
+    echowithcolor(
+      COLOR GREEN
+      "-- Dependency(${PROJECT_NAME}): civetweb found target civetweb::civetweb-c-library")
+    set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_LINK_NAME civetweb::civetweb-c-library)
+  endif()
+
+  if(TARGET civetweb::civetweb-cpp)
+    project_third_party_civetweb_patch_imported_target(civetweb::civetweb-cpp)
+  endif()
+  if(TARGET civetweb::civetweb-c-library)
+    project_third_party_civetweb_patch_imported_target(civetweb::civetweb-c-library)
+  endif()
+endmacro()
+
+if(NOT TARGET civetweb::civetweb-cpp AND NOT TARGET civetweb::civetweb-c-library)
+  find_package(civetweb QUIET CONFIG)
+  project_third_party_civetweb_import()
+  if(NOT TARGET civetweb::civetweb-cpp AND NOT TARGET civetweb::civetweb-c-library)
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_VERSION)
+      set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_VERSION "v1.14")
+    endif()
+
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_GIT_URL)
+      set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_GIT_URL
+          "https://github.com/civetweb/civetweb.git")
+    endif()
+
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_DIR)
+      project_third_party_get_build_dir(
+        ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_DIR "civetweb"
+        ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_VERSION})
+    endif()
+
+    if(NOT EXISTS ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_DIR})
+      file(MAKE_DIRECTORY ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_DIR})
+    endif()
+
+    if(NOT ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS)
+      set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS
+          "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
+          "-DCIVETWEB_BUILD_TESTING=OFF"
+          "-DCIVETWEB_ENABLE_DEBUG_TOOLS=OFF"
+          "-DCIVETWEB_ENABLE_ASAN=OFF"
+          "-DCIVETWEB_ENABLE_CXX=ON"
+          "-DCIVETWEB_ENABLE_IPV6=ON"
+          "-DCIVETWEB_ENABLE_SSL_DYNAMIC_LOADING=OFF"
+          "-DCIVETWEB_ENABLE_WEBSOCKETS=ON")
+      if(CMAKE_CROSSCOMPILING)
+        list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS
+             "-DCIVETWEB_ENABLE_SERVER_EXECUTABLE=OFF")
+      endif()
+      if(TARGET ZLIB::ZLIB)
+        list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS
+             "-DCIVETWEB_ENABLE_ZLIB=ON")
+      endif()
+      if(OPENSSL_FOUND AND NOT LIBRESSL_FOUND)
+        list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS
+             "-DCIVETWEB_ENABLE_SSL=ON")
+      else()
+
+      endif()
+      # TODO CIVETWEB_ENABLE_LUA,CIVETWEB_ENABLE_LUA_SHARED
+      list(APPEND ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS
+           "-DCIVETWEB_ENABLE_LUA=OFF")
+    endif()
+    project_third_party_append_build_shared_lib_var(
+      ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS BUILD_SHARED_LIBS)
+
+    include(AtframeworkToolsetCommonDefinitions)
+    if(ATFRAMEWORK_CMAKE_TOOLSET_SYSTEM_LINKS)
+      set(PATCH_BACKUP_CMAKE_C_STANDARD_LIBRARIES ${CMAKE_C_STANDARD_LIBRARIES})
+      set(PATCH_BACKUP_CMAKE_CXX_STANDARD_LIBRARIES ${CMAKE_CXX_STANDARD_LIBRARIES})
+      add_compiler_flags_to_var_unique(CMAKE_C_STANDARD_LIBRARIES
+                                       ${ATFRAMEWORK_CMAKE_TOOLSET_SYSTEM_LINKS})
+      add_compiler_flags_to_var_unique(CMAKE_CXX_STANDARD_LIBRARIES
+                                       ${ATFRAMEWORK_CMAKE_TOOLSET_SYSTEM_LINKS})
+    endif()
+
+    find_configure_package(
+      PACKAGE
+      civetweb
+      FIND_PACKAGE_FLAGS
+      CONFIG
+      BUILD_WITH_CMAKE
+      CMAKE_INHIRT_BUILD_ENV
+      CMAKE_INHIRT_FIND_ROOT_PATH
+      CMAKE_FLAGS
+      ${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_OPTIONS}
+      WORKING_DIRECTORY
+      "${PROJECT_THIRD_PARTY_PACKAGE_DIR}"
+      BUILD_DIRECTORY
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_BUILD_DIR}"
+      PREFIX_DIRECTORY
+      "${PROJECT_THIRD_PARTY_INSTALL_DIR}"
+      SRC_DIRECTORY_NAME
+      "civetweb-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_VERSION}"
+      GIT_BRANCH
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_VERSION}"
+      GIT_URL
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_CIVETWEB_GIT_URL}")
+    if(PATCH_BACKUP_CMAKE_C_STANDARD_LIBRARIES)
+      set(CMAKE_C_STANDARD_LIBRARIES ${PATCH_BACKUP_CMAKE_C_STANDARD_LIBRARIES})
+      unset(PATCH_BACKUP_CMAKE_C_STANDARD_LIBRARIES)
+    endif()
+    if(PATCH_BACKUP_CMAKE_CXX_STANDARD_LIBRARIES)
+      set(CMAKE_CXX_STANDARD_LIBRARIES ${PATCH_BACKUP_CMAKE_CXX_STANDARD_LIBRARIES})
+      unset(PATCH_BACKUP_CMAKE_CXX_STANDARD_LIBRARIES)
+    endif()
+
+    project_third_party_civetweb_import()
+  endif()
+else()
+  project_third_party_civetweb_import()
+endif()
+
+if(NOT TARGET civetweb::civetweb-cpp AND NOT TARGET civetweb::civetweb-c-library)
+  message(FATAL_ERROR "-- Dependency(${PROJECT_NAME}): civetweb not found")
+endif()
