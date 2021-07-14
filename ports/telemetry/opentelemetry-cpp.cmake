3ec86a1d522fa671013c94a0bbc34bc3375919d7
owent <admin@owent.net>
2021-07-14 10:49:11 +0800

    Update opentelemetry-cpp
    Signed-off-by: owent <admin@owent.net>

--- ports/telemetry/opentelemetry-cpp.cmake
+++ ports/telemetry/opentelemetry-cpp.cmake
@@ -39,7 +39,7 @@ if(NOT TARGET opentelemetry-cpp::api AND NOT TARGET opentelemetry-cpp::sdk)
 
   if(NOT TARGET opentelemetry-cpp::api AND NOT TARGET opentelemetry-cpp::sdk)
     unset(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_OPENTELEMETRY_CPP_INCLUDE_DIRECTORIES)
-    project_third_party_port_declare(opentelemetry_cpp VERSION "v1.0.0-rc2" GIT_URL
+    project_third_party_port_declare(opentelemetry_cpp VERSION "v1.0.0-rc3" GIT_URL
                                      "https://github.com/open-telemetry/opentelemetry-cpp.git")
 
     set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_OPENTELEMETRY_CPP_PATCH_FILE
