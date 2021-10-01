b2a1f4850830a3ad2852411790b1873b317c9583
owent <admin@owent.net>
2021-10-01 12:35:55 +0800

    Try to patch boringssl
    Signed-off-by: owent <admin@owent.net>

--- ports/compression/import.cmake
+++ ports/compression/import.cmake
@@ -2,6 +2,8 @@ include_guard(GLOBAL)
 
 include("${CMAKE_CURRENT_LIST_DIR}/zlib.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/lz4.cmake")
+
+# snappy depends zlib and lz4
 include("${CMAKE_CURRENT_LIST_DIR}/snappy.cmake")
 
 # zstd depends zlib and lz4
