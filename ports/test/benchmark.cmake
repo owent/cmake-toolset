5f59b7851e33064334f008a3b110847920e80cbd
owent <admin@owent.net>
2022-10-31 12:50:39 +0800

    Add `project_third_party_try_patch_file`. Update protobuf,grpc,opentelemetry-cpp
    Signed-off-by: owent <admin@owent.net>

--- ports/test/benchmark.cmake
+++ ports/test/benchmark.cmake
@@ -57,8 +57,9 @@ if(NOT TARGET benchmark::benchmark AND NOT TARGET benchmark::benchmark_main)
     # Do not use benchmark v1.7.0, which has a lot symbol exporting problems.
     project_third_party_port_declare(benchmark VERSION "v1.6.2" GIT_URL "https://github.com/google/benchmark.git")
 
-    set(ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_BENCHMARK_PATCH_FILE
-        "${CMAKE_CURRENT_LIST_DIR}/benchmark-${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_BENCHMARK_VERSION}.patch")
+    project_third_party_try_patch_file(
+      ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_BENCHMARK_PATCH_FILE "${CMAKE_CURRENT_LIST_DIR}" "benchmark"
+      "${ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_BENCHMARK_VERSION}")
 
     project_third_party_append_build_shared_lib_var(
       "benchmark" "" ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_BENCHMARK_BUILD_OPTIONS BUILD_SHARED_LIBS)
