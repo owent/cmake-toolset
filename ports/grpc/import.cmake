b096cf9d4fb4de82a96f0546b1fedf01e87f00f1
owent <admin@owent.net>
2022-11-07 10:25:28 +0800

    Optimize standalone importing settings of gRPC
    Signed-off-by: owent <admin@owent.net>

--- ports/grpc/import.cmake
+++ ports/grpc/import.cmake
@@ -7,16 +7,6 @@
 
 include_guard(GLOBAL)
 
-if(CMAKE_CROSSCOMPILING)
-  set(gRPC_BUILD_CODEGEN OFF)
-  set(gRPC_CMAKE_CROSSCOMPILING ON)
-else()
-  set(gRPC_BUILD_CODEGEN ON)
-  set(gRPC_CMAKE_CROSSCOMPILING OFF)
-endif()
-
-set(gRPC_MSVC_CONFIGURE ${CMAKE_BUILD_TYPE})
-
 include("${CMAKE_CURRENT_LIST_DIR}/abseil-cpp.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/grpc.cmake")
 
