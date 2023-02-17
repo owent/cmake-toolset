7a795b056426f375ce88e2945da65735f8e31f78
owent <admin@owent.net>
2023-02-17 16:03:48 +0800

    Patch mimalloc, enable jemalloc only when exception is available.
    Signed-off-by: owent <admin@owent.net>

--- ports/malloc/jemalloc.cmake
+++ ports/malloc/jemalloc.cmake
@@ -19,6 +19,10 @@ if(NOT TARGET jemalloc)
      AND NOT MINGW
      AND NOT TARGET jemalloc)
 
+    if(NOT COMPILER_OPTIONS_TEST_EXCEPTION)
+      message(FATAL_ERROR "Dependency(${PROJECT_NAME}): Build jemalloc require exception support.")
+    endif()
+
     project_third_party_port_declare(
       jemalloc
       VERSION
