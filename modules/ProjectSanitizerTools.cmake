b2dfd6ad73243f5fa3198e8120a21c05519d60d9
owent <owentou@tencent.com>
2024-05-13 21:25:57 +0800

    Fix configure builder with sanitizer

---
+++ modules/ProjectSanitizerTools.cmake
@@ -0,0 +1,111 @@
+# .rst: ProjectSanitizerTools
+# ----------------
+#
+# build tools
+#
+
+include_guard(GLOBAL)
+
+function(project_build_tools_sanitizer_get_name OUTPUT_VAR)
+  foreach(COMPILE_OR_LINK_FLAG ${ARGN})
+    if(COMPILE_OR_LINK_FLAG MATCHES "fsanitize=([^ \t\r\n]+)")
+      set(${OUTPUT_VAR}
+          "${CMAKE_MATCH_1}"
+          PARENT_SCOPE)
+      return()
+    endif()
+  endforeach()
+  set(${OUTPUT_VAR}
+      ""
+      PARENT_SCOPE)
+endfunction()
+
+function(project_build_tools_sanitizer_try_get_static_link OUTPUT_VAR)
+  unset(SANITIZER_NAME)
+  project_build_tools_sanitizer_get_name(SANITIZER_NAME ${ARGN})
+  if(NOT SANITIZER_NAME)
+    set(${OUTPUT_VAR}
+        ""
+        PARENT_SCOPE)
+    return()
+  endif()
+
+  string(TOUPPER "${SANITIZER_NAME}" SANITIZER_NAME_UPPER)
+  if(DEFINED ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER})
+    set(${OUTPUT_VAR}
+        "${ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER}}"
+        PARENT_SCOPE)
+    return()
+  endif()
+
+  unset(SANITIZER_TRY_STATIC_LIB)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
+    if(SANITIZER_NAME STREQUAL "address")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libasan")
+    elseif(SANITIZER_NAME STREQUAL "memory")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libmsan")
+    elseif(SANITIZER_NAME STREQUAL "undefined")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libubsan")
+    elseif(SANITIZER_NAME STREQUAL "thread")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libtsan")
+    elseif(SANITIZER_NAME STREQUAL "hwaddress")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libhwasan")
+    elseif(SANITIZER_NAME STREQUAL "dataflow")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libdfsan")
+    endif()
+  elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang|AppleClang")
+    set(SANITIZER_TRY_STATIC_LIB "-static-libsan")
+  else()
+    if(SANITIZER_NAME STREQUAL "address")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libsan" "-static-libasan")
+    elseif(SANITIZER_NAME STREQUAL "memory")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libsan")
+    elseif(SANITIZER_NAME STREQUAL "undefined")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libsan" "-static-libubsan")
+    elseif(SANITIZER_NAME STREQUAL "thread")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libsan" "-static-libtsan")
+    elseif(SANITIZER_NAME STREQUAL "hwaddress")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libsan" "-static-libhwasan")
+    elseif(SANITIZER_NAME STREQUAL "dataflow")
+      set(SANITIZER_TRY_STATIC_LIB "-static-libsan")
+    endif()
+  endif()
+
+  include(CheckCXXSourceCompiles)
+  include(CMakePushCheckState)
+
+  cmake_push_check_state()
+
+  find_package(Threads)
+  if(CMAKE_USE_PTHREADS_INIT)
+    list(APPEND CMAKE_REQUIRED_LIBRARIES Threads::Threads)
+    if(CMAKE_USE_PTHREADS_INIT)
+      set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -pthread")
+    endif()
+  endif()
+
+  set(CMAKE_REQUIRED_FLAGS_BACKUP "${CMAKE_REQUIRED_FLAGS}")
+  foreach(TRY_STATIC_LIB ${SANITIZER_TRY_STATIC_LIB})
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS_BACKUP} ${TRY_STATIC_LIB}")
+
+    unset(ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER} CACHE)
+    check_cxx_source_compiles("int main() { return 0; }"
+                              ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER})
+    if(ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER})
+      break()
+    endif()
+
+  endforeach()
+
+  cmake_pop_check_state()
+
+  if(ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER})
+    set(${OUTPUT_VAR}
+        "${ATFRAMEWORK_CMAKE_TOOLSET_SANITIZER_STATIC_LINK_${SANITIZER_NAME_UPPER}}"
+        PARENT_SCOPE)
+  else()
+    set(${OUTPUT_VAR}
+        ""
+        PARENT_SCOPE)
+  endif()
+endfunction()
