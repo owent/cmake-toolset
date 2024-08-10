ef251260787c92cb570dc2772d63dff95791a920
owent <owentou@tencent.com>
2024-08-11 00:37:43 +0800

    Fix gcc version compact

--- modules/toolchain/compiler/gnu.cmake
+++ modules/toolchain/compiler/gnu.cmake
@@ -14,15 +14,15 @@ if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "10.0.0" AND CMAKE_CXX_COMPI
 endif()
 
 # Patch for GCC 14.1
-if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "14.0.0" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "14.1.0")
+if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "14.0.0" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "14.2.0")
   add_compiler_flags_to_inherit_var_unique(CMAKE_CXX_FLAGS "-Wno-error=incompatible-pointer-types")
   add_compiler_flags_to_inherit_var_unique(CMAKE_CXX_FLAGS "-Wno-error=implicit-int")
   add_compiler_flags_to_inherit_var_unique(CMAKE_C_FLAGS "-Wno-error=incompatible-pointer-types")
   add_compiler_flags_to_inherit_var_unique(CMAKE_C_FLAGS "-Wno-error=implicit-int")
 endif()
 
-# Patch for GCC 14.1
-if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "14.0.0" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "14.1.0")
+# Patch for GCC 14.2
+if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "14.2.0" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "14.3.0")
   add_compiler_flags_to_inherit_var_unique(CMAKE_CXX_FLAGS "-Wno-error=uninitialized")
   add_compiler_flags_to_inherit_var_unique(CMAKE_CXX_FLAGS "-Wno-error=maybe-uninitialized")
   #[[
