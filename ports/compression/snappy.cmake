15398987ce6c4519df925748c4fae0031d1e354d
owent <admin@owent.net>
2026-09-15 15:30:22 +0800

    Update ports

--- ports/compression/snappy.cmake
+++ ports/compression/snappy.cmake
@@ -23,7 +23,7 @@ if(NOT TARGET Snappy::snappy)
       PORT_PREFIX
       COMPRESSION
       VERSION
-      "1.2.2"
+      "1.3.0"
       GIT_URL
       "https://github.com/google/snappy.git"
       BUILD_OPTIONS
